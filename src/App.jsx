3fb057a31da018fcafe4ab9fc907f9d8a388e057
Nurebi Uraimov <nurebi2007@gmail.com>
2025-02-13 12:14:50 +0600

    Save

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import UserName from "./components/UI/userName"
+
+function App() {
+return (
+<>
+<UserName name="add"/>
+<UserName name="create"/>
+</>
+)
+}
+
+export default App
---
+++ src/components/UI/userName.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const UserName = (props) => {
+  return (
+    <div>
+        <button>{props.name}</button>
+        <button>{props.name}</button>
+    </div>
+  )
+}
+
+export default UserName
